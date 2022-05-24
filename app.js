b4e09ae40184ee71143ad5bd0d4267d418ae4bd3
Ori Guy <origuydev@gmail.com>
2022-05-24 22:41:47 +0300

    .

--- app.js
+++ app.js
@@ -1,38 +1,42 @@
-const btn = document.querySelector("#btn")
+const btn = document.querySelector("#btn");
 let quoteText = document.querySelector("#quoteGenerator");
 let authorText = document.querySelector("#author");
 let loaderAnimation = document.querySelector("#loaderAnimation");
 let mainContainer = document.querySelector("#mainContainer");
 let share = document.querySelector("#share");
-let textAreaArray = [quoteText,authorText];
+let textAreaArray = [quoteText, authorText];
 
 let updateSite = (data) => {
-    quoteText.innerText = data[0].quote
-    authorText.innerText = data[0].author
-    stop()
-}
+  quoteText.innerText = data[0].quote;
+  authorText.innerText = data[0].author;
+  stop();
+};
 let getQuoteFromApi = () => {
-    start()
-    fetch("https://api.breakingbadquotes.xyz/v1/quotes").then(response => response.json()).then(data =>updateSite(data))
-
-}
+  start();
+  fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+    .then((response) => response.json())
+    .then((data) => updateSite(data));
+};
 function start() {
-    loaderAnimation.style.visibility = "visible"
-    mainContainer.style.visibility = "hidden"
+  loaderAnimation.style.visibility = "visible";
+  mainContainer.style.visibility = "hidden";
 }
 function stop() {
-    loaderAnimation.style.visibility = "hidden"
-    mainContainer.style.visibility = "visible"
+  loaderAnimation.style.visibility = "hidden";
+  mainContainer.style.visibility = "visible";
 }
-btn.addEventListener("click",getQuoteFromApi)
+btn.addEventListener("click", getQuoteFromApi);
 share.addEventListener("click", () => {
-    FB.ui({
-        display: 'popup',
-        method: 'share',
-        href: 'https://unique-quote-generator.netlify.app/',
-      }, function(response){
-          console.log(response)
-      })
-})
+  FB.ui(
+    {
+      display: "popup",
+      method: "share",
+      href: "https://unique-quote-generator.netlify.app/",
+    },
+    function (response) {
+      console.log(response);
+    }
+  );
+});
 
-stop()
+stop();
